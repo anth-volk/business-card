37e84f841aee38c03ed632303dcaccaf566b010a
Anthony Volk <anth.volk@gmail.com>
2023-01-05 15:55:19 +0800

    Added styling and spacing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+// External imports
+import React, { useState } from 'react';
+
+// Internal imports
 import './App.css';
+/*
+import './img/headshot.png';
+*/
 
 function App() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function displaySettings() {};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      {isDarkMode === true && <div className="darkMode" />}
+      <section className="ImageContainer">
+        {/* Under const}
+        <img src="./img/headshot.png" />
+  {*/}
+      </section>
+      <section className="InfoContainer">
+        <div className="InfoHead">
+          <h1>Anthony Volk</h1>
+          <h2 className="InfoHead_position">Aspiring Fullstack Developer</h2>
+          <a className="InfoHead_link" href="http://www.anthonyvolk.com" target="_blank" rel="noreferrer"><p>anthonyvolk.com</p></a>
+          <div className="ButtonWrapper">
+            <a className="ButtonEmail" href="mailto:anth.volk@gmail.com"><i class="fa-solid fa-envelope"></i><p>Email</p></a>
+            <a className="ButtonLinkedIn" href="https://linkedin.com/in/anthonyvolk"><i class="fa-brands fa-linkedin"></i><p>LinkedIn</p></a>
+          </div>
+        </div>
+        <div className="InfoBody">
+          <h2>About Me</h2>
+          <p>I'm an aspiring fullstack developer with an interest in ethical and just tech that empowers individuals and changes communities. I'm currently looking for full-time and freelance work where I can use my tech skills to make an impact.</p>
+          <h2>Interests</h2>
+          <p>Ethical tech and justice. Global history. Cultures and languages. Politics and society. Hiking and long walks. </p>
+        </div>
+        <div className="InfoFooter">
+          <a className="ButtonFooter" href="https://github.com/anth-volk"><i className="fa-brands fa-square-github"></i></a>
+          <a className="ButtonFooter" href="https://codesandbox.io/u/anth-volk"><i className="fa-regular fa-square"></i></a>
+          <div className="ButtonFooter" onClick={displaySettings}><i className="fa-solid fa-gear"></i></div>
+        </div>
+      </section>
+    </main>
   );
 }
 
